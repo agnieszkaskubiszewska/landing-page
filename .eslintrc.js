162ad226d1e94c9f679e4596503d4b27c672911e
AgS <129868680+agnieszkaskubiszewska@users.noreply.github.com>
2025-03-23 13:42:53 +0100

    project clean (#1)
    Co-authored-by: Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['playwright'],
-  extends: ['plugin:playwright/recommended'],
+  plugins: ['playwright', 'prettier'],
+  extends: ['plugin:playwright/recommended', 'plugin:prettier/recommended'],
   rules: {
-    'playwright/expect-expect': ['error', {
-      assertFunctionNames: ['expect', 'expect*', 'page.waitForURL']
-    }],
+    'playwright/expect-expect': [
+      'error',
+      {
+        assertFunctionNames: ['expect', 'expect*', 'page.waitForURL'],
+      },
+    ],
     'playwright/no-force-option': 'warn',
     'playwright/no-wait-for-timeout': 'warn',
     'playwright/no-conditional-in-test': 'warn',
-    'playwright/no-networkidle': 'warn'
-  }
-}; 
+    'playwright/no-networkidle': 'warn',
+    'prettier/prettier': 'error',
+  },
+};
--- playwright.config.ts
+++ playwright.config.ts
@@ -21,4 +21,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-}); 
+});
